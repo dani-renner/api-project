2967f60c22f4b3c78e067d2516ab8dd216713bd8
Dani Renner <danijrenner@gmail.com>
2021-02-08 11:32:58 -0800

    add wind speed result & zip search
    Co-authored-by: Geof Rosenmund <geof.rosenmunds.email@gmail.com>

--- docs/main.js
+++ docs/main.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     $('#location').val("");
 
     let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}&units=imperial`;
 
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
@@ -21,8 +21,38 @@ $(document).ready(function() {
     request.send();
 
     function getElements(response) {
+      console.log(response);
       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+      $('.showTemp').text(`The temperature in Fahrenheit is ${parseInt(response.main.temp)} degrees.`);
+      $('.showWind').text(`The wind speed is ${response.wind.speed} mph.`);
+      // const rain = response.rain && response.rain["1h"]
     }
   });
+  
+  $('#zipcodeLocation').click(function() {
+    const zipcode = $('#zipcode').val();
+    $('#zipcode').val("");
+
+    let request = new XMLHttpRequest();
+      const url = `http://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&appid=${process.env.API_KEY}&units=imperial`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
+
+    function getElements(response) {
+      console.log(response);
+      $('.showHumidity').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Fahrenheit is ${parseInt(response.main.temp)} degrees.`);
+      $('.showWind').text(`The wind speed is ${response.wind.speed} mph.`);
+      // const rain = response.rain && response.rain["1h"]
+    }
+  });
+
 });
