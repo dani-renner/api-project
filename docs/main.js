ce0c680b59e5b096359d91bf7954e392d3e6c49e
Dani Renner <danijrenner@gmail.com>
2021-02-08 12:02:44 -0800

    add try & catch for practice
    Co-authored-by: Geof Rosenmund <geof.rosenmunds.email@gmail.com>

--- docs/main.js
+++ docs/main.js
@@ -30,8 +30,32 @@ $(document).ready(function() {
   });
   
   $('#zipcodeLocation').click(function() {
-    const zipcode = $('#zipcode').val();
+    const zipcode = parseInt($('#zipcode').val());
     $('#zipcode').val("");
+
+    function checkNumber(number) {
+      if (isNaN(number) || number < 0) {
+        return new Error("Not a valid number!");
+      } else {
+        return true;
+      }
+    }
+
+    try {
+      const isNumberValid = checkNumber(zipcode);
+      if (isNumberValid instanceof Error) {
+        console.error(isNumberValid.message);
+        throw RangeError("Not a valid number!");
+      } else {
+        console.log("Try was successful, so no need to catch!");
+        $('#showErrors').text("This number is valid. You may continue.")
+      }
+      } catch(error) {
+        console.error(`Red alert! We have an error: ${error.message}`)
+      }
+      
+    
+
 
     let request = new XMLHttpRequest();
       const url = `http://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&appid=${process.env.API_KEY}&units=imperial`;
